6d29c0361cb82a333f2a0d3affea2156ee880760
Charangadade <91818305+Charangadade@users.noreply.github.com>
2024-05-16 09:45:15 +0530

    Add files via upload

---
+++ Login_page.cs
@@ -0,0 +1,87 @@
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using selenium_practice1.Test_util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//*[contains(@class,'modal-innerl')]
+
+namespace selenium_practice1.Pages
+{
+     public class Login_page
+    {
+        public string username = "//*[@id=\"login\"]/div/div/form/div[2]/input";
+        public string password = "//*[@id=\"passwordInput\"]";
+        public string loginbutton ="//*[@id='login']/div/div/form/div[4]/button";
+        public string errormessage = "//*[@id=\"login\"]/div/div/form/div[1]/ul/li";
+        public string mainspace1 = "//*[@id=\"unified-modere\"]/modere-unified-main/ng-component/div[1]/div/div/footer";
+        public string mainspace = "//div[@id='promo-ribbon']/following-sibling::section/div[contains(@class,'modal-inner')]/a[contains(@class,'modal-close-icon')]";
+        public string popscreen = "//*[@id='promo-modal']/div";
+
+        public string donotshow = "//*[@id=\"promoSection\"]/p/a[@data-close='Close']";
+       
+        public IWebDriver driver;
+
+        public WebDriverUtility wd;
+        public WebDriverWait wait;
+        public Login_page(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10000));
+            wd = new WebDriverUtility(driver);
+            wd.NavigateToPage("https://www.modere.co.in/");
+            wd.SetPageLoadTimeout(10000);
+        }
+        public void NavigateToPage(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+        /*public bool IsElementClickable(By locator)
+        {
+            try
+            {
+                wait.Until(ExpectedConditions.ElementToBeClickable(locator));
+                return true;
+            }
+            catch(WebDriverTimeoutException)
+            {
+                return false;
+            }
+        }*/
+        public IWebElement mainSpace => wd.FindElement(By.XPath(mainspace));
+        public void clearPopScreen()
+        {
+            Console.WriteLine("clear pop screen");
+            // wd.ClickElement(By.XPath(mainspace));
+            //mainSpace.Click();
+            wd.ScrollToElement(By.XPath(donotshow));
+            // IWebElement donot => wd.FindElement(By.XPath(donotshow));
+            wd.ClickElement(By.XPath(donotshow));
+        }
+
+        public void EnterUsername(string Username)
+        {
+           // IWebElement usernameField=wd.FindElement(By.XPath(username));
+            // usernameField.SendKeys(Username);
+            wd.SendKeysToElement(By.XPath(username), Username);
+        }
+        public void EnterPassword(string Password)
+        {
+           // IWebElement passwordField = wd.FindElement(By.XPath(password));
+            //passwordField.SendKeys(Password);
+            wd.SendKeysToElement(By.XPath(password), Password);
+        }
+        public void ClickLoginButton()
+        {
+           // IWebElement loginButton = wd.FindElement(By.XPath(loginbutton));
+            wd.ClickElement(By.XPath(loginbutton));
+        }
+        public string GetErrorMessage()
+        {
+            IWebElement errorMessage = wd.FindElement(By.XPath(errormessage));
+            return errorMessage.ToString();
+        }
+    }
+}
---
+++ Login_tests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using selenium_practice1.Pages;
+using selenium_practice1.Test_util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Legacy;
+
+namespace selenium_practice1.Tests
+{
+    [TestFixture]
+     class Login_tests
+    {
+        private IWebDriver driver;
+        private Login_page loginPage;
+
+        //public WebDriverUtility wd;
+
+        [SetUp]
+        public void TestSetup()
+        {
+
+            driver = new ChromeDriver();
+            loginPage = new Login_page(driver);
+            // cartPage.NavigateToPage("https://www.modere.co.in");
+            //wd = new WebDriverUtility(driver);
+           // wd.NavigateToPage("https://www.modere.co.in");
+           // wd.SetPageLoadTimeout(10000);
+
+        }
+        [Test]
+        public void TestInvalidLogin()
+        {
+            loginPage.clearPopScreen();
+            loginPage.EnterUsername("siri");
+            loginPage.EnterPassword("1234");
+            loginPage.ClickLoginButton();
+            ClassicAssert.AreEqual("Invalid Login / Email or Password", 
+                loginPage.GetErrorMessage);
+              // Console.Write(loginPage.GetErrorMessage());
+        }
+        [Test]
+        public void TestValidLogin()
+        {
+            loginPage.clearPopScreen();
+            loginPage.EnterUsername("sireeshasiddi25@gmail.com");
+            loginPage.EnterPassword("Sirisha.nana25");
+            loginPage.ClickLoginButton();
+            ClassicAssert.AreEqual("https://www.modere.co.in/", driver.Url);
+
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+    }
+}
